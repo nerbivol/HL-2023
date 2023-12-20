1c91eeff98d37ba6055d61c545678d254cb1fc07
Serhii Biruk <68056715+nerbivol@users.noreply.github.com>
2023-12-20 19:08:45 +0200

    Update ProcessUnits.java

--- 4.Hadoop/ProcessUnits.java
+++ 4.Hadoop/ProcessUnits.java
@@ -1,88 +1,77 @@
-package hadoop;
+package hadoop; 
 
-import java.util.*;
+import java.util.*; 
 
-import java.io.IOException;
-import java.io.IOException;
+import java.io.IOException; 
+import java.io.IOException; 
 
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.conf.*;
-import org.apache.hadoop.io.*;
+import org.apache.hadoop.io.FloatWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapred.*;
-import org.apache.hadoop.util.*;
 
-public class ProcessUnits
-{
-   //Mapper class
-   public static class E_EMapper extends MapReduceBase implements
-   Mapper<LongWritable ,/*Input key Type */
-   Text,                /*Input value Type*/
-   Text,                /*Output key Type*/
-   IntWritable>        /*Output value Type*/
-   {
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.StringTokenizer;
 
-      //Map function
-      public void map(LongWritable key, Text value,
-      OutputCollector<Text, IntWritable> output,
-      Reporter reporter) throws IOException
-      {
-         String line = value.toString();
-         String lasttoken = null;
-         StringTokenizer s = new StringTokenizer(line,"\t");
-         String year = s.nextToken();
+public class ProcessUnits {
+    public static class E_EMapper extends MapReduceBase
+            implements Mapper<LongWritable, Text, Text, FloatWritable> {
 
-         while(s.hasMoreTokens())
-            {
-               lasttoken=s.nextToken();
+        public void map(LongWritable key, Text value,
+                        OutputCollector<Text, FloatWritable> output,
+                        Reporter reporter) throws IOException {
+            String line = value.toString();
+            StringTokenizer s = new StringTokenizer(line, "\t");
+            String year = s.nextToken();
+            float val;
+
+            while (s.hasMoreTokens()) {
+                val = Float.parseFloat(s.nextToken());
+                output.collect(new Text(year), new FloatWritable(val));
             }
 
-         int avgprice = Integer.parseInt(lasttoken);
-         output.collect(new Text(year), new IntWritable(avgprice));
-      }
-   }
+        }
+    }
 
 
-   //Reducer class
-   public static class E_EReduce extends MapReduceBase implements
-   Reducer< Text, IntWritable, Text, IntWritable >
-   {
+    public static class E_EReduce extends MapReduceBase implements
+            Reducer<Text, FloatWritable, Text, FloatWritable> {
 
-      //Reduce function
-      public void reduce( Text key, Iterator <IntWritable> values,
-         OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException
-         {
-            int maxavg=30;
-            int val=Integer.MIN_VALUE;
+        public void reduce(Text key, Iterator<FloatWritable> values,
+                           OutputCollector<Text, FloatWritable> output, Reporter reporter) throws IOException {
+            float count = 0;
+            float sum = 0;
+            while (values.hasNext()) {
+                float val = values.next().get();
 
-            while (values.hasNext())
-            {
-               if((val=values.next().get())>maxavg)
-               {
-                  output.collect(key, new IntWritable(val));
-               }
+                sum += val;
+                count++;
+
             }
+            float avg = sum / count;
+            output.collect(key, new FloatWritable(avg));
 
-         }
-   }
+        }
+    }
 
 
-   //Main function
-   public static void main(String args[])throws Exception
-   {
-      JobConf conf = new JobConf(ProcessUnits.class);
+    public static void main(String[] args) throws Exception {
+        JobConf conf = new JobConf(ProcessUnits.class);
 
-      conf.setJobName("max_eletricityunits");
-      conf.setOutputKeyClass(Text.class);
-      conf.setOutputValueClass(IntWritable.class);
-      conf.setMapperClass(E_EMapper.class);
-      conf.setCombinerClass(E_EReduce.class);
-      conf.setReducerClass(E_EReduce.class);
-      conf.setInputFormat(TextInputFormat.class);
-      conf.setOutputFormat(TextOutputFormat.class);
+        conf.setJobName("avg_eletricity_units");
+        conf.setOutputKeyClass(Text.class);
+        conf.setOutputValueClass(FloatWritable.class);
+        conf.setMapperClass(E_EMapper.class);
+        conf.setCombinerClass(E_EReduce.class);
+        conf.setReducerClass(E_EReduce.class);
+        conf.setInputFormat(TextInputFormat.class);
+        conf.setOutputFormat(TextOutputFormat.class);
 
-      FileInputFormat.setInputPaths(conf, new Path(args[0]));
-      FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+        FileInputFormat.setInputPaths(conf, new Path(args[0]));
+        FileOutputFormat.setOutputPath(conf, new Path(args[1]));
 
-      JobClient.runJob(conf);
-   }
+        JobClient.runJob(conf);
+    }
 }
